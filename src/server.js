85d96124fa550cc15a46caa20862d0a88e4c927b
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-19 16:21:17 -0300

    feat: Added Setups and Server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import { MongoClient, ObjectId } from 'mongodb';
-import dotenv from 'dotenv';
-import joi from 'joi';
-import dayjs from 'dayjs';
-dotenv.config();
+import router from './routes/index.js';
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
+server.use(router);
 
 server.listen(process.env.PORT, () => console.log(`Server running in port: ${process.env.PORT}`));
