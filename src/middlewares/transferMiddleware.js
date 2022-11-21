06b58dbd0c743c25d0ebd3b7c36436969aa9951c
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-21 01:11:36 -0300

    feat: transferMiddleware.js added

---
+++ src/middlewares/transferMiddleware.js
@@ -0,0 +1,12 @@
+import transferSchema from '../schemas/transferSchema.js';
+
+export function transferMiddleware(req, res, next) {
+  const { value, description, type } = req.body;
+
+  const validation = transferSchema.validate({ value, description, type });
+  if (validation.error) {
+    return res.sendStatus(422);
+  }
+
+  next();
+}
