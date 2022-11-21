b3696cc317809cb10015388db50b327cfd00382f
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-21 00:35:26 -0300

    feat: added tranferRouter to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
+import transferRouter from "./transferRouter.js";
 
 const router = Router();
 router.use(authRouter);
+router.use(transferRouter);
 
 export default router;
