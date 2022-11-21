ce54f9727270d70b90b3c0f5e9682da054335f59
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-21 00:43:47 -0300

    feat: tranferRouter.js added

---
+++ src/routes/transferRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { transferAdded, transfersListed } from '../controllers/transferController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { transferMiddleware } from '../middlewares/transferMiddleware.js';
+
+const transferRouter = express.Router();
+transferRouter.post('/transfer', authMiddleware, transferMiddleware, transferAdded);
+transferRouter.get('/transfer', authMiddleware, transfersListed);
+
+export default transferRouter;
