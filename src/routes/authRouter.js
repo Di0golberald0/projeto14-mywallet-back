8889f9c500dbbba4667564d5ef7cb1506e617752
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-22 17:12:48 -0300

    fix: removed authMiddleware from authRouter sign-in

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { signUp, signIn } from '../controllers/authController.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { userMiddleware } from '../middlewares/userMiddleware.js';
 
 const authRouter = Router();
